7232ce49be4b43b405e6d53593f36fa5968b275d
Julien Vanier <jvanier@gmail.com>
2015-03-30 13:22:04 -0400

    Add dotenv for environment variables

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').load();
+
 // If settings present, start New Relic
 if (process.env.NEW_RELIC_APP_NAME && process.env.NEW_RELIC_LICENSE_KEY) {
   console.log('Activating New Relic: ', process.env.NEW_RELIC_APP_NAME);
